42a3e9544d426e7f02dc6dd92adf5c3922a425bc
Ubuntu <ubuntu@ip-172-31-3-79.ap-northeast-1.compute.internal>
2015-06-18 14:49:02 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,20 @@
 cd `dirname $0`
 
 # Delete all containers
-docker rm $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
 # Delete all images
-docker rmi $(docker images -q)
+#docker rmi $(docker images -q)
 
 # ビルド
-docker build --no-cache --rm -t elasticsearch-img:0.1.0 .
+docker build --rm -t elasticsearch-img:0.1.0 .
+#docker build --no-cache --rm -t elasticsearch-img:0.1.0 .
 
 # 前回起動中のものがあれば削除
 docker stop elasticsearch
 docker rm elasticsearch
 
 # リンクして実行
-docker run -d -p 9200:9200 --name elasticsearch --link mysql:mysql elasticsearch-img:0.1.0
+docker run -d -p 9200:9200 --name elasticsearch elasticsearch-img:0.1.0
 
 sleep 10
 
